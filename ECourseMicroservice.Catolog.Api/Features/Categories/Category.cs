42fc247f26b938dd9aac046936e1397c940c503e
OzanUlus <ilkerozanulus@gmail.com>
2024-11-29 15:46:34 +0300

    entity and dbcontext

---
+++ ECourseMicroservice.Catolog.Api/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using ECourseMicroservice.Catolog.Api.Features.Courses;
+using ECourseMicroservice.Catolog.Api.Repositories;
+
+namespace ECourseMicroservice.Catolog.Api.Features.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public List<Course>? Courses { get; set; }
+    }
+}
---
+++ ECourseMicroservice.Catolog.Api/Repositories/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ECourseMicroservice.Catolog.Api.Features.Categories;
+using ECourseMicroservice.Catolog.Api.Features.Courses;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System.Reflection;
+
+namespace ECourseMicroservice.Catolog.Api.Repositories
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
